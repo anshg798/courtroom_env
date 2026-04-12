5be4bd7b449cbd51ef2b8dd3b00be5d582f4572a
Ansh Gupta <finindanshmulmill@gmail.com>
2026-04-12 23:15:09 +0530

    updated main callback

--- server/app.py
+++ server/app.py
@@ -98,4 +98,4 @@ if __name__ == "__main__":
     parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
-    main(host=args.host, port=args.port)
+    main()
